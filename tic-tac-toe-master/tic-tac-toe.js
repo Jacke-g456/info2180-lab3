a9352cab8536f65d35a60cc4e8d8a7e8fc82c12d
Jacke-g456 <steviecool2014@gmail.com>
2025-10-17 19:38:27 -0500

    Add an X or O to a square when cliecked

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const board = document.getElementById('board');
     const squares = board.querySelectorAll('div');
+    var current_player = "X";
+    let state = ['', '', '', '', '', '', '', '', ''];
     
 
     //Adds the square class to all the div items inside the div with id board
@@ -8,4 +10,39 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let i = 0; i < squares.length; i++) {
         squares[i].classList.add('square');
     }
+
+    squares.forEach((square,i) => {
+        square.addEventListener('click', function(){
+            //check if the click is on an empty square
+            if (state[i]==''){
+                //Update the state
+                state[i]= current_player;
+
+                //Update the visual dispaly
+                square.textContent=current_player;
+                square.classList.add(current_player);
+
+               
+                 // Switch player for next turn
+                if (current_player=='X'){
+                    current_player="O";
+                }
+                else{
+                    current_player="X";
+                }
+                console.log(current_player);
+
+
+
+            }
+
+        
+        });
+        
+    });
+
+
+
+
+
 });
